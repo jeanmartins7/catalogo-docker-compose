f20694c31c35c425a90344cf62b8e7e7e797339f
jeanverso <jeanmarcel8@gmail.com>
2026-01-11 16:18:34 -0300

    chore: create jenkins compose

--- Jenkins/vault-start.sh
+++ Jenkins/vault-start.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 VAULT_PATH="$1"
+HOST_GIT_DIR=$HOST_GIT_DIR
+HOST_BACKUP_DIR=$VAULT_NAS_DIR
 
 if [ -z "$VAULT_PATH" ]; then
     echo "❌ Uso: ./vault-start.sh <CAMINHO_DO_SEGREDO>"
     echo "Exemplo: ./vault-start.sh jeanverso/data/jenkins"
+    exit 1
+fi
+
+if [ -z "$HOST_GIT_DIR" ]; then
+    echo "❌ ERRO: A variável de ambiente HOST_GIT_DIR não está definida."
+    exit 1
+fi
+
+if [ -z "$HOST_BACKUP_DIR" ]; then
+    echo "❌ ERRO: A variável de ambiente VAULT_NAS_DIR não está definida."
     exit 1
 fi
 
@@ -36,8 +48,10 @@ echo "📝 [ENV] Gerando arquivo .env..."
 
 echo "$DATA_BLOCK" | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' > .env
 
-echo "   - Variáveis injetadas:"
+echo "HOST_GIT_DIR=$HOST_GIT_DIR" >> .env
+echo "HOST_BACKUP_DIR=$HOST_BACKUP_DIR" >> .env
 
+echo "   - Variáveis injetadas:"
 cat .env | cut -d '=' -f 1 | sed 's/^/   * /'
 
 echo "🚀 [DOCKER] Iniciando serviço..."
